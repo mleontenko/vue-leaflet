c123e65fc478086d9ba777a2ae01c61757cddb8d
Marin Leontenko <marin.leontenko@gmail.com>
2021-10-25 15:46:11 +0200

    init empty map

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+   <div id="mapContainer"></div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+ name: "Map",
+ data() {
+   return{
+     center: [0, 0]
+   }},
+ methods: {
+   setupLeafletMap: function () {
+    const mapDiv = L.map("mapContainer").setView(this.center, 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapDiv);
+   },
+ },
+ mounted() {
+   this.setupLeafletMap();
+ },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#mapContainer {
+ width: 80vw;
+ height: 100vh;
 }
 </style>
